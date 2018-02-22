5ede2ee46091a121d04f5ee238f5154fb2319085
logonrm <logonrm@fiap.com.br>
2018-02-21 23:00:45 -0300

    Inserindo imagens

---
+++ app/src/main/java/com/persistere/pokeagenda/model/Pokemon.kt
@@ -0,0 +1,3 @@
+package com.persistere.pokeagenda.model
+
+data class Pokemon(var name: String, var sprites: Sprite)
---
+++ app/src/main/java/com/persistere/pokeagenda/model/Sprite.kt
@@ -0,0 +1,6 @@
+package com.persistere.pokeagenda.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Sprite(@SerializedName("front_default")
+                  var frontDefault: String)
